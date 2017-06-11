95e1633e0183f1db0eba1034314a208ab52c6ee9
min <shuiwuzhilan@sina.com>
2017-06-11 11:17:36 +0800

    Add files via upload

--- sources.sh
+++ sources.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+sourcesfile="https://raw.githubusercontent.com/zhimengyun/Debian/master/sources.list"
 
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
-wget https://raw.githubusercontent.com/zhimengyun/Debian/master/sources.list
-cp ./sources.list /etc/apt/sources.list
+if [ -f "./sources.list" ];
+then
+  echo "wget sources.list~"
+  wget $sourcesfile
+  sleep 0.1
+
+  echo "backup sources.list.bak~"
+  cp /etc/apt/sources.list /etc/apt/sources.list.bak
+  sleep 0.1
+
+  echo "replace sources.list~"
+  cp ./sources.list /etc/apt/sources.list
+fi
