acf2c884895dc26aefb32028d7ac84c15d7914a0
zhimengyun <shuiwuzhilan@sina.com>
2017-07-22 19:26:13 +0800

    Add files via upload

---
+++ bashrc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+sourcesfile="https://raw.githubusercontent.com/zhimengyun/Debian/8.x/.bashrc"
+filename=$(basename $sourcesfile)
+
+#验证权限
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root!" 1>&2
+	exit 1
+fi
+
+if [ "$(whoami)" != "root" ]; then
+	echo "This script must be run as root!!" 1>&2
+	exit 1
+fi
+
+if [ "$HOME" != "/root" ]; then
+	echo "This script must be run under /root!!!" 1>&2
+	exit 1
+fi
+
+# echo $sourcesfile
+if [ -f "~/.bashrc" ];
+then
+  echo "wget .bashrc~"
+  wget $sourcesfile -O "$filename"
+  sleep 0.1
+
+  echo "backup .bashrc.bak~"
+  mv ~/.bashrc ~/.bashrc.bak
+  sleep 0.1
+
+  echo "replace .bashrc~"
+  cp ./.bashrc ~/.bashrc
+else
+  echo "wget .bashrc~"
+  wget $sourcesfile -O "$filename"
+  sleep 0.1
+
+  echo "cp .bashrc~"
+  cp ./.bashrc ~/.bashrc
+fi
